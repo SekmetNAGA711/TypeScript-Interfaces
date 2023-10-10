b8be0af1c4c1a184f2ed91dd5b7b85755be753a5
AbbyANG <bena.ao310@gmail.com>
2023-10-10 14:24:35 -0400

    finished interface

---
+++ GolfCoach.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.GoalfCoach = void 0;
+class GoalfCoach {
+    getDailyWorkout() {
+        return "Hit 100 balls at the golf range.";
+    }
+}
+exports.GoalfCoach = GoalfCoach;
